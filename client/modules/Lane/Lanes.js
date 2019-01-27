dbaf9adf1594c355ada64a3bc2d257926733f7d0
Rafal Leszczynski <info@grafikart.pl>
2019-01-27 22:11:53 +0100

    update endpoints

--- client/modules/Lane/Lanes.js
+++ client/modules/Lane/Lanes.js
@@ -1,4 +1,6 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
+import Lane from './LaneContainer.js';
 import Lane from './Lane.js';
 
 const Lanes = ({ lanes }) => {
